7ff2bdd36fea6c155429bb0a1634d9b8194cf601
student <student@virtualbox>
2017-11-04 12:50:41 +0100

    initial commit

---
+++ src/main/java/jdbcdemo/jdbcdemo/App.java
@@ -0,0 +1,13 @@
+package jdbcdemo.jdbcdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
