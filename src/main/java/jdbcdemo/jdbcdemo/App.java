620d45c89300040c5b7ab6d100de90068acc3d43
student <student@virtualbox>
2017-11-04 13:51:54 +0100

    Praca z SQL

--- src/main/java/jdbcdemo/jdbcdemo/App.java
+++ src/main/java/jdbcdemo/jdbcdemo/App.java
@@ -1,13 +1,25 @@
 package jdbcdemo.jdbcdemo;
-
+import java.util.List;
+import domain.Person;
+import jdbcdemo.dao.PersonRepository;
 /**
- * Hello world!
- *
- */
-public class App 
+* Hello world!
+*
+*/
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public static void main( String[] args )
+{
+PersonRepository repository = new PersonRepository();
+Person janek = new Person();
+janek.setName("Jan");
+janek.setSurname("Kowalski");
+janek.setAge(30);
+repository.add(janek);
+List<Person> people = repository.getAll();
+for(Person p : people){
+System.out.println(p.getId()+"\t"+p.getName()+"\t"+p.getSurname()+"\t"+p.getAge());
+}
+System.out.println( "Koniec" );
+}
 }
